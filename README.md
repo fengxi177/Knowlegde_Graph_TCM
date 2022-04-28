ca31c9b2b698bb257256adac10fd07bf3bdf8854
fengxi177 <cqtoshtots2021@163.com>
2022-04-28 09:52:38 +0800

    中药知识图谱-子模块demo

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Knowlegde_Graph_TCM
-中医药知识图谱demo
+# 中医药知识图谱demo
+    说明：为保证本项目每一目录都是可单独运行的子模块，可能有部分代码冗余
+
+## 中药知识图谱-子模块
+   目录：zhongyao
+   内容：代码、数据(数据质量总体较高，部分待提升)
+   中药知识图谱概述：请查看zhongyao/img_svg/中药知识图谱demo节点关系概述.png
+   ![中药知识图谱demo节点关系概述图](https://github.com/fengxi177/Knowlegde_Graph_TCM/zhongyao/img_svg/中药知识图谱demo节点关系概述.png)
+   
+
---
+++ zhongyao/main_zhongyao.py
@@ -0,0 +1,82 @@
+# -*- coding = utf-8 -*-
+
+"""
+    author:fengxi
+    time:2022/4/28 8:17
+
+    project name:Knowlegde_Graph_TCM
+    file name:main_zhongyao.py
+    function:
+        中药知识图谱demo
+"""
+import json
+from py2neo import Graph, Node, Relationship
+
+
+def generateGraph_Node(graph, label, name):
+    """
+        创建知识图谱节点
+    :param graph: Graph()
+    :param label: 节点label
+    :param name: 节点name
+    :return:
+    """
+
+    node = Node(label, name=name)
+    graph.create(node)
+
+    return node
+
+
+def generateGraph_Relation(graph, node_1, relation, node_2):
+    """
+        连接知识图谱关系
+    :param graph:Graph()
+    :param node_1: 头实体节点
+    :param relation: 关系
+    :param node_2: 尾实体节点
+    :return:
+    """
+
+    r = Relationship(node_1, relation, node_2)
+    graph.create(r)
+
+
+def create_graph_zhongyao():
+    """
+        创建中药知识图谱，在neo4j中进行可视化
+    :return:
+    """
+
+    nodes = []
+    # 加载关系数据
+
+    # === 连接知识图谱
+    connect_graph = Graph("http://localhost:7474", auth=("neo4j", "123456"))
+
+    # === 加载节点数据,创建节点
+    # 创建关系时索引节点
+    dict_nodes = {}  # key: 节点lable\tname, value:生成的图节点
+    with open("./data_zhongyao/nodes_zhongyao.txt", "r", encoding="utf-8") as fr_n:
+        for line in fr_n.readlines():
+            line = line.strip()
+            lable, name = line.split("\t")
+            # 创建节点
+            node = generateGraph_Node(connect_graph, lable, name)
+            dict_nodes[line] = node
+
+    # === 加载关系数据
+    with open("./data_zhongyao/relations_zhongyao.json", "r", encoding="utf-8") as fr_r:
+        for ele in json.load(fr_r):
+            node_1 = ele["node_1"]
+            relation = ele["relation"]
+            node_2 = ele["node_2"]
+            node_1_g = dict_nodes[node_1]
+            node_2_g = dict_nodes[node_2]
+            # 创建关系
+            generateGraph_Relation(connect_graph, node_1_g, relation, node_2_g)
+
+
+if __name__ == '__main__':
+    create_graph_zhongyao()
+    pass
